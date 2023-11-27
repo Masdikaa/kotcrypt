0271ccddb2de55e931b5d6b253a4d36db3d935fb
Masdikaa <masdikailhanmansiz@gmail.com>
2023-11-27 11:45:31 +0700

    # Functionalities - ViewBinding - SplashScreen

--- app/src/main/java/com/masdika/kotcrypt/MainActivity.kt
+++ app/src/main/java/com/masdika/kotcrypt/MainActivity.kt
@@ -2,10 +2,17 @@ package com.masdika.kotcrypt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.masdika.kotcrypt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Thread.sleep(3000)
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
